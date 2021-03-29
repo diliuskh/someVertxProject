f8c94f881e7fe4bf36b345d14ea3737368a81670
diliuskh <diliuskh@gmail.com>
2021-03-29 19:27:44 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,21 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin ("jvm") version "1.4.21"
-  application
-  id("com.github.johnrengelman.shadow") version "6.1.0"
+    kotlin("jvm") version "1.4.32"
+    application
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "net.dilius"
 version = "1.0.0-SNAPSHOT"
 
 repositories {
-  mavenCentral()
-  jcenter()
+    mavenCentral()
+    jcenter()
 }
 
-val vertxVersion = "4.0.0"
-val junitJupiterVersion = "5.7.0"
+val vertxVersion = "4.0.3"
+val junitJupiterVersion = "5.7.1"
 
 val mainVerticleName = "net.dilius.some_vertx.MainVerticle"
 val launcherClassName = "io.vertx.core.Launcher"
@@ -26,51 +26,53 @@ val watchForChange = "src/**/*"
 val doOnChange = "${projectDir}/gradlew classes"
 
 application {
-  mainClassName = launcherClassName
+    mainClass.set(launcherClassName)
+    @Suppress("DEPRECATION")
+    mainClassName = launcherClassName
 }
 
 dependencies {
-  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
-  implementation("io.vertx:vertx-web-client")
-  implementation("io.vertx:vertx-web-validation")
-  implementation("io.vertx:vertx-health-check")
-  implementation("io.vertx:vertx-web")
-  implementation("io.vertx:vertx-zookeeper")
-  implementation("io.vertx:vertx-grpc")
-  implementation("io.vertx:vertx-micrometer-metrics")
-  implementation("io.vertx:vertx-auth-oauth2")
-  implementation("io.vertx:vertx-config")
-  implementation("io.vertx:vertx-web-graphql")
-  implementation("io.vertx:vertx-web-templ-pebble")
-  implementation("io.vertx:vertx-pg-client")
-  implementation("io.vertx:vertx-web-sstore-cookie")
-  implementation("io.vertx:vertx-circuit-breaker")
-  implementation("io.vertx:vertx-lang-kotlin-coroutines")
-  implementation("io.vertx:vertx-kafka-client")
-  implementation("io.vertx:vertx-lang-kotlin")
-  implementation(kotlin("stdlib-jdk8"))
-  testImplementation("io.vertx:vertx-junit5")
-  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+    implementation("io.vertx:vertx-web-client")
+    implementation("io.vertx:vertx-web-validation")
+    implementation("io.vertx:vertx-health-check")
+    implementation("io.vertx:vertx-web")
+    implementation("io.vertx:vertx-zookeeper")
+    implementation("io.vertx:vertx-grpc")
+    implementation("io.vertx:vertx-micrometer-metrics")
+    implementation("io.vertx:vertx-auth-oauth2")
+    implementation("io.vertx:vertx-config")
+    implementation("io.vertx:vertx-web-graphql")
+    implementation("io.vertx:vertx-web-templ-pebble")
+    implementation("io.vertx:vertx-pg-client")
+    implementation("io.vertx:vertx-web-sstore-cookie")
+    implementation("io.vertx:vertx-circuit-breaker")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines")
+    implementation("io.vertx:vertx-kafka-client")
+    implementation("io.vertx:vertx-lang-kotlin")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("io.vertx:vertx-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions.jvmTarget = "11"
 
 tasks.withType<ShadowJar> {
-  archiveClassifier.set("fat")
-  manifest {
-    attributes(mapOf("Main-Verticle" to mainVerticleName))
-  }
-  mergeServiceFiles()
+    archiveClassifier.set("fat")
+    manifest {
+        attributes(mapOf("Main-Verticle" to mainVerticleName))
+    }
+    mergeServiceFiles()
 }
 
 tasks.withType<Test> {
-  useJUnitPlatform()
-  testLogging {
-    events = setOf(PASSED, SKIPPED, FAILED)
-  }
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(PASSED, SKIPPED, FAILED)
+    }
 }
 
 tasks.withType<JavaExec> {
-  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+    args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
 }
